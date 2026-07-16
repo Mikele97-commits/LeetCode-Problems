0a041b21351ca1460fbcfe3a6d9e02f1b73e0698
Mikele <michal.minich@o2.pl>
2026-07-16 16:04:30 +0200

    Add solutions to problems 1431 and 605

--- src/LeetCode75.java
+++ src/LeetCode75.java
@@ -5,5 +5,11 @@ public class LeetCode75 {
 
         //1071. Greatest Common Divisor of Strings. https://leetcode.com/problems/greatest-common-divisor-of-strings/description
         //System.out.println(StringExercises.gcdOfStrings("AAAAAAAAA","AAA"));
+
+        //1431. Kids With the Greatest Number of Candies. https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/
+        //System.out.println(ArrayExercises.kidsWithCandies(new int[]{1,2,3,4}, 2));
+
+        //605. Can Place Flowers. https://leetcode.com/problems/can-place-flowers/description/
+        //System.out.println(ArrayExercises.canPlaceFlowers(new int[]{0,1,0,1}, 1));
     }
 }
